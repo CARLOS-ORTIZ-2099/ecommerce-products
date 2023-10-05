75af1619d81e8882c090175564e16f2c93b994c4
Carlos Eduardo Ortiz <carlosklinton.2016@gmail.com>
2023-10-04 23:51:04 -0500

    se creo la logica para la ventana modal y se aplico estilos

--- src/components/gallery-product/GalleryProduct.jsx
+++ src/components/gallery-product/GalleryProduct.jsx
@@ -1,41 +1,132 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './gallery-product.css'
-
+import { Modal } from './Modal'
 
 export const GalleryProduct = ({product}) => {
-    
-    const [imagenS, setImagen] = useState([])
-    const [valor, setValor] = useState(null)
+    console.log(product)
+    const [imagenState, setImagenState] = useState([])
+    const [indice, setIndice] = useState(null)
 
-    const showModal = (imagen, indice) => {
-        setValor(indice)
-        setImagen(imagen)
-        console.log(indice, imagen)
+ // logica para mostrar el modal
+const showModal = (imagen, indice) => {
+        setIndice(indice)
+        setImagenState(imagen)
+}
+
+ // logica para ocultar el modal
+const closeModal = () => {
+        setIndice(null)
+}
+
+// logica para rotar las imagenes a la derecha
+
+const nextImage = () => {
+    let sum = indice + 1 
+
+    let productLength = product.gallery.length 
+
+    if(sum  > productLength-1){
+        let cero = 0
+        let result = product.gallery.find((ele, index) => index == cero)
+        setImagenState(result)
+        setIndice(cero)
+        return
     }
+   
+   let result = product.gallery.find((ele, index) => index == sum)
+   console.log(result)
+   setImagenState(result)
+   setIndice(sum)
 
+}
+
+// logica para rotar las imagenes a la izquierda
+
+const previusImage = () => {
+    
+    let less = indice - 1 
+
+    let productLength = product.gallery.length 
+
+    if(less  < 0){
+     
+        let result = product.gallery.find((ele, index) => index == productLength-1)
+        setImagenState(result)
+        setIndice(productLength-1)
+        return
+    }
+   
+   let result = product.gallery.find((ele, index) => index == less)
+   console.log(result)
+   setImagenState(result)
+   setIndice(less)
+ 
+ }
 
   return (
-
             <>
-
                 <div className='gallery-container'>
                     {
-                    product?.gallery?.map((imagen, index) => (
-                        <div key={index}>
-                            <img onClick={() => showModal(imagen, index)} className='img-modal' src={`${imagen.img}`} alt="" />
-                        </div>
-                    ))
+                        product?.gallery?.map((imagen, index) => (
+                            <div key={index}>
+                                <img onClick={() => showModal(imagen, index)} className='img-modal'
+                                src={`${imagen.img}`}
+                                alt="" 
+                                />
+                            </div>
+                        ))
                     }
                 </div> 
 
-  
                     {
-                        valor && <div>
-                                <img src={`${imagenS.img}`} alt="" />
-                        </div>
+                        indice !== null && 
+                        <Modal closeModal={closeModal} 
+                               imagenState={imagenState}
+                               nextImage= {nextImage}
+                               previusImage={previusImage}
+                               setIndice={setIndice}
+                        />
                     }
             </>
-    
   )
 }
+                       
+
+/* import React, { useState } from 'react';
+import './gallery-product.css';
+
+export const GalleryProduct = ({ product }) => {
+  const [valor, setValor] = useState(null);
+
+  const showModal = (imagen, indice) => {
+    setValor(indice);
+  };
+
+  const closeImage = () => {
+    setValor(null); // Esto cierra la imagen al hacer clic en ella nuevamente
+  };
+
+  return (
+    <>
+      <div className="gallery-container">
+        {product?.gallery?.map((imagen, index) => (
+          <div key={index}>
+            <img
+              onClick={() => showModal(imagen, index)}
+              className="img-modal"
+              src={`${imagen.img}`}
+              alt=""
+            />
+          </div>
+        ))}
+      </div>
+
+      {valor !== null && (
+        <div className="lightbox" onClick={closeImage}>
+          <img src={`${product?.gallery?.[valor]?.img}`} alt="" />
+        </div>
+      )}
+    </>
+  );
+}; */
---
+++ src/components/gallery-product/Modal.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+import './modal.css'
+
+export const Modal = ({closeModal, imagenState, nextImage, previusImage, setIndice}) => {
+
+    const hiddenModal = () => {
+        setIndice(null)
+    }
+
+  return (
+    
+    <div  className='border border-5 border-primary modal-custom'>
+      
+
+        <img className="img-fluid" src={`${imagenState.img}`} alt="" />
+        <div>
+            <i className='previus bi bi-arrow-left-circle-fill fs-1' onClick={previusImage}></i>
+            <i className='next bi bi-arrow-right-circle-fill fs-1' onClick={nextImage}></i>
+        </div>
+        <i className='close bi bi-x-circle-fill fs-1' onClick={hiddenModal}></i>
+       
+    </div>
+
+  )
+
+
+}
